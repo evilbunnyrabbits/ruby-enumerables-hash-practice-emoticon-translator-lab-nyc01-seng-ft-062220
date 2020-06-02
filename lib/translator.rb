92dca97de9c94a93ff91c921877b5c1bee683598
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-06-02 14:58:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,21 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+
+def load_library(file)
+  array = YAML.load(File.read(file))
+  result = array.each_with_object({}) do |(key, value), final_array|
+      final_array
+      if !final_array[key]
+        final_array[key] = {}
+        final_array[key][:english] = value[0]
+        final_array[key][:japanese] = value[1]
+      end
+  end
+  return result
 end
+
 
 def get_japanese_emoticon
   # code goes here
